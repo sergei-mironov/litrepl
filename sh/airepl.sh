783a1627ae1c7c4ed6d0d13c67dffecf1bd0c176
Sergei Mironov <sergei.v.mironov@proton.me>
2024-07-18 15:12:49 +0400

    Add airepl.sh

---
+++ sh/airepl.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo "WARNING: THIS SCRIPT EXECUTES AI-GENERATED PROGRAMS!" >&2
+
+litrepl restart ai
+(
+echo "/model \"~/.local/share/nomic.ai/GPT4All/Meta-Llama-3-8B-Instruct.Q4_0.gguf\""
+echo "Please be very brief! How can I convert 'input.gif' to 'output.webm' using ffmpeg oneliner?"
+) | tee >(cat 1>&2) \
+  | litrepl eval-code ai \
+  | tee >(cat 1>&2) \
+  | sed -n 's/^`\?\(ffmpeg[^`]*\)`\?$/\1/p' \
+  >_cmd.sh
+
+while ! (
+        rm output.webm 2>/dev/null ; \
+        test -s _cmd.sh || { echo "Please suggest an ffmpeg oneliner!" >&2 ; exit 1; } && \
+        sh _cmd.sh && \
+        test -s output.webm || { echo "OUTPUT FILE IS EMPTY!" >&2 ; exit 1; } ; \
+        ) 2>_err.txt ; do
+  (
+  echo $'Nope! The output was:\n\n'
+  cat _err.txt | tail
+  echo $'\n\nPlease make another attempt!'
+  ) | tee >(cat 1>&2) \
+    | litrepl eval-code ai \
+    | tee >(cat 1>&2) \
+    | sed -n 's/^`\?\(ffmpeg[^`]*\)`\?$/\1/p' \
+    > _cmd.sh
+done
