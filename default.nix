997ef0ad26d039226d77e831bf47d6f10c44407f
Sergei Mironov <sergei.v.mironov@proton.me>
2024-12-28 21:14:48 +0400

    Update paper

--- default.nix
+++ default.nix
@@ -199,7 +199,7 @@ let
           pdflscape environ trimspaces mdframed zref needspace import
           beamerposter qcircuit xypic standalone preview amsmath thmtools
           tocloft tocbibind varwidth beamer tabulary ifoddpage relsize svg
-          transparent biber biblatex;
+          transparent biber biblatex comment;
         }
       );
 
