d2c7e136d7d3902beb367b8e9ad5f1d2c905bae3
Sergei Mironov <sergei.v.mironov@proton.me>
2025-09-11 21:41:01 +0400

    Update install_deps_ubuntu.sh

--- sh/install_deps_ubuntu.sh
+++ sh/install_deps_ubuntu.sh
@@ -3,4 +3,4 @@ set -e -x
 
 sudo apt-get update >/dev/null
 sudo apt-get install -y socat vim
-pip install setuptools tqdm coverage coverage_badge
+pip install setuptools tqdm coverage coverage_badge lark psutil
